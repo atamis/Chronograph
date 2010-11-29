d41fa0f77579bf91151db3082d13a114448fca7b
Indigo Casson <atamiser@gmail.com>
2010-11-28 19:44:53 -0800

    Added the group field, and removed the inheritance from Struct, as it wasn't needed.

--- lib/chronograph/event.rb
+++ lib/chronograph/event.rb
@@ -1,13 +1,15 @@
 module Chronograph
-    class Event < Struct.new(:name, :date, :desc)
-    attr_accessor :name, :date, :desc
-        def initialize(name, date, desc)
+    class Event
+        attr_accessor :name, :date, :desc, :group
+        def initialize(name, date, desc, group=:default)
             raise TypeError.new("date must be a date") unless date.is_a? Date
             raise TypeError.new("name has to be a name or symbol") unless name.is_a?(String) || name.is_a?(Symbol)
             raise TypeError.new("desc must be a string") unless desc.is_a? String
+            raise TypeError.new("group has to be a name or symbol") unless group.is_a?(String) || group.is_a?(Symbol)
             @name = name
             @date = date
             @desc = desc
+            @group = group
         end
     end
     
